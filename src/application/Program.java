138817b425003fe8657267d51bb860fa83e9fa03
Ana Julia <borgesanajulia55@gmail.com>
2022-11-09 13:52:33 +0000

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,10 +4,11 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import boardgame.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+
+import boardgame.ChessException;
 
 
 public class Program {
@@ -16,31 +17,35 @@ public class Program {
 		
 		Scanner sc = new Scanner (System.in);
 		ChessMatch chessMatch = new ChessMatch();
-				
-		while(true) {
-		try {
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.println("Source:  ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.println("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-		}
+
 		
-		catch (ChessException e ) {
-			System.out.println(e.getMessage());
-			sc.nextLine();
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
-	    catch (InputMismatchException e ) {
-	    	System.out.println(e.getMessage());
-	    	sc.nextLine();
-	    }
 		
      }
 	}
-}
+
